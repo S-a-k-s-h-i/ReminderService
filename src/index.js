e0c5faa338e1918eb8f42290f6b7b6c8e013e6ee
S-a-k-s-h-i <sakshipokhria1997@gmail.com>
2023-01-09 15:54:43 +0530

    Added NotificationTicket model

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ const bodyParser = require("body-parser");
 
 const { PORT } = require("./config/serverConfig");
 
-const { sendBasicEmail } = require("./services/email-service");
+const cron = require("node-cron");
+// const { sendBasicEmail } = require("./services/email-service");
 
 const app = express();
 
@@ -11,12 +12,15 @@ const setupAndStartServer = () => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.listen(PORT, () => {
-    sendBasicEmail(
-      "support@admin.com",
-      "movieticketservice@gmail.com",
-      "This is a testing mail",
-      "Hey, how are you, I hope you like the support"
-    );
+    // sendBasicEmail(
+    //   "support@admin.com",
+    //   "movieticketservice@gmail.com",
+    //   "This is a testing mail",
+    //   "Hey, how are you, I hope you like the support"
+    // );
+    cron.schedule("*/1 * * * *", () => {
+      console.log("running a task every two minutes");
+    });
     console.log(`Server listening on port: ${PORT}`);
   });
 };
